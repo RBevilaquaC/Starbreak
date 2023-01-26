f49b03f692db1393bdc1fb09140dc2f1f9a27205
Amós do Carmo Moreira Filho <adcmf.snf20@uea.edu.br>
2023-01-26 17:44:20 -0400

    Add Timeline Controller

---
+++ Assets/Timeline/TimelineController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+     private PlayableDirector director;
+     public GameObject controlPanel;
+    
+    private void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+        director.played += Director_Played;
+        director.stopped += Director_Stopped;
+    }
+    
+    private void Director_Stopped(PlayableDirector obj)
+    {
+        controlPanel.SetActive(true);
+    }
+
+    private void Director_Played(PlayableDirector obj)
+    {
+        controlPanel.SetActive(false);
+    }
+
+    public void StartTimeline()
+    {
+        director.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
